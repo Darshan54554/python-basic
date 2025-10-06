75cca70f06ba3faefd9a30a0d29f6c9755cd4f47
Darshan54554 <darshansnair2020@gmail.com>
2025-10-06 22:46:13 +0530

    Create task2.py

---
+++ DAY 1/task2.py
@@ -0,0 +1,5 @@
+name=input("enter your name")
+age=input("enter you age")
+language=input("enter your language")
+intro=f"hey i'm {name} ,i'm {age} years old, i love coding in {language}"
+print(intro)
