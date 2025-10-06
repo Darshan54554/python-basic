a0b0e4d021fbdd7bd73fcbc76ee7af46ef576f70
Darshan54554 <darshansnair2020@gmail.com>
2025-10-06 22:48:39 +0530

    Create task5.py

---
+++ DAY 1/task5.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name: ").strip().title()  
+language = input("Enter your favorite programming language: ").strip().upper()  
+
+
+intro = f"Hey, I'm {name}, and I love coding in {language}!"
+print(intro)
