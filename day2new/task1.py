9e4f61a2ac3a0175a23d2d676cebce87d6967e31
Darshan54554 <darshansnair2020@gmail.com>
2025-10-13 10:22:50 +0530

    Update task1.py

--- day2new/task1.py
+++ day2new/task1.py
@@ -1,1 +1,7 @@
-
+mood=input("how are you feeling ? (happy/sad):")
+if mood=="happy":
+    print("that's awesome!")
+elif mood=="sad":
+    print("here is a virtual hug")
+else:
+    print("lets make your day better") 
